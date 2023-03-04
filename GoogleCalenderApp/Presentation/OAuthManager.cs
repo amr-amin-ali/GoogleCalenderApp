a8470e6e275fe1a2402479b417727aa7458390a6
Amr Amin <amramin7@hotmail.com>
2023-03-04 13:24:59 +0200

    Use appsettings.json for OAuth

--- GoogleCalenderApp/Presentation/OAuthManager.cs
+++ GoogleCalenderApp/Presentation/OAuthManager.cs
@@ -8,11 +8,11 @@ namespace GoogleCalenderApp.Presentation
 {
     public class OAuthManager : IOAuthManager
     {
-        //private readonly string _oAuthClientId = Models.AppSettings.GetAppSettings().OAuthClientId;
-        //private readonly string _oAuthClientSecret = Models.AppSettings.GetAppSettings().OAuthClientSecret;
+        private readonly string _oAuthClientId = Models.AppSettings.GetAppSettings().OAuthClientId;
+        private readonly string _oAuthClientSecret = Models.AppSettings.GetAppSettings().OAuthClientSecret;
         private readonly ILogger _logger;
-        private readonly string _oAuthClientId = "995426710029-kjmq32lmdpn03js5pepuqi70uortavji.apps.googleusercontent.com";
-        private readonly string _oAuthClientSecret = "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";
+        //private readonly string _oAuthClientId = "995426710029-kjmq32lmdpn03js5pepuqi70uortavji.apps.googleusercontent.com";
+        //private readonly string _oAuthClientSecret = "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";
 
         private readonly string[] _scopes = {
             "https://www.googleapis.com/auth/calendar",
